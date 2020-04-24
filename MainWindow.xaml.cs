6b00eaa6b583a197ec265d5be3392d6c6939d81c
HaciIsma <haciismayilov5@outlook.com>
2020-04-24 11:44:45 +0400

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Timer_MVVM
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ViewModel/ViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Threading;
+
+namespace Timer_MVVM.ViewModel
+{
+    public class ViewModel : ViewModelBase
+    {
+        private string _currentTime;
+
+        public DispatcherTimer _timer;
+
+        public string CurrentTime
+        {
+            get
+            {
+                return _currentTime;
+            }
+            set
+            {
+                if (_currentTime == value)
+                    return;
+                _currentTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ViewModel()
+        {
+            _timer = new DispatcherTimer(DispatcherPriority.Render);
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += (sender, args) =>
+            {
+                CurrentTime = DateTime.Now.ToLongTimeString();
+            };
+            _timer.Start();
+        }
+    }
+}
